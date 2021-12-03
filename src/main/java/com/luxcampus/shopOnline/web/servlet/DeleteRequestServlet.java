155bd3da330623a5146a359c4b46480d6a9273f2
Clio18 <estclio09@gmail.com>
2021-12-04 01:11:16 +0200

    On-line Shop: functionality was added (sql schema was changed to fixed issue with unique name of product, mapping was fixed)

---
+++ src/main/java/com/luxcampus/shopOnline/web/servlet/DeleteRequestServlet.java
@@ -0,0 +1,43 @@
+package com.luxcampus.shopOnline.web.servlet;
+
+import com.luxcampus.shopOnline.entity.Product;
+import com.luxcampus.shopOnline.service.ProductService;
+import com.luxcampus.shopOnline.web.util.PageGenerator;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Map;
+
+public class DeleteRequestServlet extends HttpServlet {
+    private ProductService productService;
+
+    public DeleteRequestServlet(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        PageGenerator pageGenerator = PageGenerator.instance();
+        String page = pageGenerator.getPage("deleteProduct.html");
+        resp.getWriter().write(page);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        try {
+            int id = Integer.parseInt(req.getParameter("id"));
+            System.out.println(id);
+            productService.delete(id);
+            resp.sendRedirect("/products");
+        }catch (Exception e){
+            String errorMessage = "Your product has not been deleted! Please try again";
+            PageGenerator pageGenerator = PageGenerator.instance();
+            Map<String, Object> parameters = Map.of("errorMessage", errorMessage);
+            String page = pageGenerator.getPage("deleteProduct.html", parameters);
+            resp.getWriter().write(page);
+        }
+    }
+}
---
+++ src/main/java/com/luxcampus/shopOnline/web/servlet/UpdateRequestServlet.java
@@ -0,0 +1,45 @@
+package com.luxcampus.shopOnline.web.servlet;
+
+import com.luxcampus.shopOnline.entity.Product;
+import com.luxcampus.shopOnline.service.ProductService;
+import com.luxcampus.shopOnline.web.util.PageGenerator;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Map;
+
+public class UpdateRequestServlet extends HttpServlet {
+    private ProductService productService;
+
+    public UpdateRequestServlet(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        PageGenerator pageGenerator = PageGenerator.instance();
+        String page = pageGenerator.getPage("updateProduct.html");
+        resp.getWriter().write(page);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        try {
+            int id = Integer.parseInt(req.getParameter("id"));
+            String name = req.getParameter("name");
+            double price = Double.parseDouble(req.getParameter("price"));
+            Product product = Product.builder().id(id).name(name).price(price).build();
+            productService.update(product);
+            resp.sendRedirect("/products");
+        }catch (Exception e){
+            String errorMessage = "Your product has not been updated! Please try again";
+            PageGenerator pageGenerator = PageGenerator.instance();
+            Map<String, Object> parameters = Map.of("errorMessage", errorMessage);
+            String page = pageGenerator.getPage("updateProduct.html", parameters);
+            resp.getWriter().write(page);
+        }
+    }
+}
